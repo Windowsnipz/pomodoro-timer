dab563ed4c6ea2432dc4fe386b58a7ed90f1ee1c
windowsnipz <nstrickland2014@gmail.com>
2024-08-03 09:17:34 -0400

    progress on components

---
+++ src/Clock.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Clock() { 
+    return (
+        <>
+            <div>
+                <h1>Pomodor Timer</h1>
+            </div>
+            <div id="clock-wrapper">
+                <div id="break-label">
+                    <div>
+                        <h3>Break Length</h3>
+                    </div>
+                    <div>
+                        <button id="break-decrement">Decrement</button>
+                        <h3 id="break-length"></h3>
+                        <button id="break-increment">Increment</button>
+                    </div>
+                </div>
+                <div id="session-label">
+                    <div>
+                        <h3>Session Length</h3>
+                    </div>
+                    <div>
+                        <button id="session-decrement">Decrement</button>
+                        <h3 id="session-length"></h3>
+                        <button id="session-increment">increment</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Clock;
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav id="navbar">
+            <img alt="logo image" src="https://avatars.githubusercontent.com/u/146682460?v=4" />
+            <div id="nav-links">
+                <a href="#">Homepage</a>
+                <a href="#">Projects</a>
+                <a href="#">Hire Me</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
