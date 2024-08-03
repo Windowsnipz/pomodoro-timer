0db4ca57ece7038fa5a7c62fde3f468ed5c0bab5
windowsnipz <nstrickland2014@gmail.com>
2024-08-03 09:08:16 -0400

    progress on Clock component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar';
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Clock />
     </div>
   );
 }
