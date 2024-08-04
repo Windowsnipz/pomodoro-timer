f23a149a2e7d6677b04e3a6b30419c24bd1ac4bf
windowsnipz <nstrickland2014@gmail.com>
2024-08-04 08:10:52 -0400

    Clocksettings component

---
+++ src/ClockSettings.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function ClockSettings() {
+    return (
+        <>
+            <h1>Pomodoro Timer</h1>
+            <div id="break-label">
+                <div>
+                    <h3>Break Length</h3>
+                </div>
+                <div>
+                    <button id="break-decrement">Decrement</button>
+                    <h3 id="break-length"></h3>
+                    <button id="break-increment">Increment</button>
+                </div>
+            </div>
+            <div id="session-label">
+                <div>
+                    <h3>Session Length</h3>
+                </div>
+                <div>
+                    <button id="session-decrement">Decrement</button>
+                    <h3 id="session-length"></h3>
+                    <button id="session-increment">increment</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default ClockSettings;
