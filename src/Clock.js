6ee67e4c0d6d09279bf4e8c07a97f6669e8aeeff
windowsnipz <nstrickland2014@gmail.com>
2024-08-04 09:18:59 -0400

    added terneraries for formatting... Hopefully it will help

--- src/Clock.js
+++ src/Clock.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
 function Clock({ sessionLength, breakLength, changeSessionLength, changeBreakLength }) { 
+    
+    let minutes = sessionLength;
+    let seconds = 0;
 
     const reset = () => {
         changeSessionLength(25);
         changeBreakLength(5);
     }
 
+    const sessionClockTicking = () => {
+
+    }
+
+    const breakClockTicking = () => {
+
+    }
+
     return (
         <>
             <div>
                 <h3 id="timer-label">Session</h3>
-                <h1 id="time-left"></h1>
+                <h1 id="time-left">{minutes < 10 ? "0" + minutes : minutes}:{seconds < 10 ? "0" + seconds : seconds }</h1>
             </div>
             <div>
-                <button id="start_stop">Start/Stop</button>
+                <button id="start_stop" onClick={sessionClockTicking}>Start/Stop</button>
                 <button id="reset" onClick={reset}>Reset</button>
             </div>
         </>
