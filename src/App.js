4f8a272c972cc2dc3976909409fd741375f2d159
windowsnipz <nstrickland2014@gmail.com>
2024-08-04 08:10:00 -0400

    restructured some components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './Navbar';
+import ClockSettings from './ClockSettings';
 import Clock from './Clock';
 
 function App() { // Reminder: put Navbar back in
   return (
     <div className="App">
-      
+      <ClockSettings />
       <Clock />
     </div>
   );
--- src/Clock.js
+++ src/Clock.js
@@ -4,37 +4,12 @@ function Clock() {
     return (
         <>
             <div>
-                <h1>Pomodoro Timer</h1>
+                <h3 id="timer-label">Session</h3>
+                <h1 id="time-left"></h1>
             </div>
-            <div id="clock-wrapper">
-                <div id="break-label">
-                    <div>
-                        <h3>Break Length</h3>
-                    </div>
-                    <div>
-                        <button id="break-decrement">Decrement</button>
-                        <h3 id="break-length"></h3>
-                        <button id="break-increment">Increment</button>
-                    </div>
-                </div>
-                <div id="session-label">
-                    <div>
-                        <h3>Session Length</h3>
-                    </div>
-                    <div>
-                        <button id="session-decrement">Decrement</button>
-                        <h3 id="session-length"></h3>
-                        <button id="session-increment">increment</button>
-                    </div>
-                </div>
-                <div>
-                    <h3 id="timer-label">Session</h3>
-                    <h1 id="time-left"></h1>
-                </div>
-                <div>
-                    <button id="start_stop">Start/Stop</button>
-                    <button id="reset">Reset</button>
-                </div>
+            <div>
+                <button id="start_stop">Start/Stop</button>
+                <button id="reset">Reset</button>
             </div>
         </>
     );
