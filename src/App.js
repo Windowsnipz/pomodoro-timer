2b58d8cfef810a8f1c9a6002868ad5014d4994cf
windowsnipz <nstrickland2014@gmail.com>
2024-08-04 08:31:33 -0400

    more structure change. Now using state in the App component with hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import React, { useState } from 'react';
 import Navbar from './Navbar';
 import ClockSettings from './ClockSettings';
 import Clock from './Clock';
 
 function App() { // Reminder: put Navbar back in
+  const [sessionLength, setSessionLength] = useState(25);
+  const [breakLength, setBreakLength] = useState(5);
+
   return (
     <div className="App">
-      <ClockSettings />
-      <Clock />
+      <ClockSettings 
+        defaultSessionLength={sessionLength}
+        defaultBreakLength={breakLength} 
+        changeSessionLength={setSessionLength}
+        changeBreakLength={setBreakLength}
+        />
+      <Clock sessionLength={sessionLength} breakLength={breakLength} />
     </div>
   );
 }
